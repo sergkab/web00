7c696161fb5c752ca0740cdf32a28497ee6bc2c6
sergkab <sergkab@gmail.com>
2022-05-31 22:40:57 +0300

    change spider

---
+++ spider/js/temp.js
@@ -0,0 +1,12 @@
+
+let myvar = '2'
+let imya = `pole${myvar}`;
+console.log(imya);
+
+/*
+let data = [11, 22, 33];
+const str = `Number ${data[0]} from data`;
+console.log(str); // Number 11 from data
+*/
+
+
