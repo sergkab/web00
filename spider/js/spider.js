f673b17a1cd458eb2c72b556acfccc8b8f9b5b2a
sergkab <sergkab@gmail.com>
2022-06-05 22:14:41 +0300

    in spider rasklad kart ver 01

--- spider/js/spider.js
+++ spider/js/spider.js
@@ -14,56 +14,33 @@ let mascards = [[0,0,0,0,0,11],
     [0,0,0,0,94],
     [0,0,0,0,111]];   // [ [прав верх,,,прав низ]... ]
 
-let masobjk = ['0','','','','',''];
+let masobjk = ['0','','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    '','','','','','','','','','',
+    ];
 
 let infoline = document.querySelector('#infoline');
 infoline.innerHTML = mascards[0][5];
 
 
-//  вывод карт во вспомог окно
-function vyvodkart(){
-    let needshow = true;
-    let ryad = 0;
-    let stroki = ['r0 ','r1 ','r2 ','r3 ','r4 ','r5 '];
-    while (needshow) {
-        needshow = false;
-        for ( let kolon = 0; kolon<10; kolon++ ) {
-            if ( mascards[kolon].length>ryad ) {
-                needshow = true;
-                stroki[ryad] += mascards[kolon][ryad]+' ';
-            }
-        }
-        if (needshow) console.log(stroki[ryad]);
-        ryad++;
-    }
-}   //  vyvodkart
-
-//vyvodkart();
-
-
-  //document.body.append(div);
-
-
-//  отображение карт
-let div0 = document.createElement('div');
-
-let kolon = document.querySelector('#col0');
-kolon.append(div0);
-div0.className = "card";
-div0.innerHTML = "A";
-let div1 = document.createElement('div');
-kolon = document.querySelector('#col1');
-kolon.append(div1);
-div1.className = "card";
-div1.innerHTML = "B";
 
 function formirkart(){
     let index_cart;
     let tempstr;
-    let imyadiv;
+    let smeschV = 5;
+    let smeschVn = 200;
+    let smesch = '';
     let pole_2;
     let index_mul;
-    for ( index_cart=1; index_cart<6; index_cart++) {
+    for ( index_cart=1; index_cart<106; index_cart++) {
         //tempstr = '#karta'+index_cart;
         //imyadiv = 'divk'+index_cart;
         masobjk[index_cart] = document.createElement('div');
@@ -74,27 +51,15 @@ function formirkart(){
         index_mul = index_cart*3;
         tempstr = `${index_mul}px`;
         masobjk[index_cart].style.left = tempstr;
-        console.log(index_cart, tempstr);
+        smesch = `${smeschV}`;
+        if (index_cart>55) {smesch=`${smeschVn}`};
+        masobjk[index_cart].style.top = smesch+"px";
+        console.log(index_cart, tempstr, smesch);
     }
 }
 
 formirkart();
 
-//let proba1 = document.createElement('div'); 
-let proba1 = document.querySelector('.colon2');
-//proba1.style.position = 'absolute';
-proba1.innerHTML = '1-1';
-proba1.style.height = "100px";
-proba1.style.width = "40px";
-
-/*
-for ( let cnt = 0; cnt<9; cnt++) {
-    let kolon = document.querySelector('#col'+cnt);
-    kolon.append(div);
-    div.className = "card";
-    div.innerHTML = "A";
-}
-*/
 
 
 
